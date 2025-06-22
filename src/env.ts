1d4e76f1dd4b7599a38bb71efb8f36380a555dc1
Taynan Breno <thaynanbreno@gmail.com>
2025-06-22 15:34:53 -0300

    feat: add passport and jwt module

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   // NODE_ENV: z.enum(['development', 'test', 'production']),
   PORT: z.coerce.number().default(3333),
-  // JWT_SECRET: z.string(),
+  JWT_SECRET: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
