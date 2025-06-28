dd24ca3c97d4e598edf312ba33f3723a8992a8dc
Taynan Breno <thaynanbreno@gmail.com>
2025-06-28 11:44:59 -0300

    feat: add create question controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,25 +1,60 @@
-import { Controller, HttpCode, Post, UseGuards } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
 
-// const createQuestionBodySchema = z.object({
-//   email: z.string().email(),
-//   password: z.string(),
-// })
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
 
-// type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
-  @HttpCode(200)
-  // @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user)
-    return 'CreateQuestionController'
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
+  async handle(
+    @Body() body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+    const userId = user.sub
+    const slug = this.convertToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    })
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase() // Convert to lowercase
+      .normalize('NFD') // Normalize Unicode characters (e.g., accented characters)
+      .replace(/[\u0300-\u036f]/g, '') // Remove accents
+      .trim() // Remove leading/trailing whitespace
+      .replace(/[^a-z0-9 -]/g, '') // Remove non-alphanumeric characters (except spaces and hyphens)
+      .replace(/\s+/g, '-') // Replace spaces with hyphens
+      .replace(/-+/g, '-') // Collapse multiple hyphens into a single hyphen
+      .replace(/^-+|-+$/g, '') // Remove leading/trailing hyphens
   }
 }
